55e19cfd9fab24feac3ab1fe0b6b6cecfde5dfe3
Jacob Ward <jacobwarduk@users.noreply.github.com>
2015-10-15 12:42:03 +0100

    More dev work

--- public/scripts/tawe.js
+++ public/scripts/tawe.js
@@ -1,4 +1,6 @@
 // Function to toggle responsive menu
 function toggleMenu() {
-    document.querySelector(".menu").classList.toggle("menu-open");
-}
+    document.querySelector(".menu")
+    .classList
+    .toggle("menu-open");
+};
